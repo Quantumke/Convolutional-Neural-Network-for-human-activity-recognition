6e7133d4dbd173f04a05fff62b1bcdb13e3e2db5
quantumke <nguruben@gmail.com>
2016-10-05 09:18:27 +0300

    mormalized data and plotted axis

--- index.py
+++ index.py
@@ -11,11 +11,24 @@ class track_humans():
     def __init__(self, *args, **kwargs):
         self.author="Ben Nguru"
         self.read_data()
+        self.normalize_dt()
+        self.plot(ax,x,y,title)
 
     def read_data(file_dir):
         column_names = ['user-id','activity','timestamp', 'x-axis', 'y-axis', 'z-axis']
         data = pd.read_csv(file_path,header = None, names = column_names)
         return data
+    def normalize_dt(dataset):
+        mean_dataset=np.mean(dataset, axis=0)
+        sigma=np.std(dataset, axis =0)
+        return (dataset-nean_dataset)/sigma
+    def plot(ax,x,y,title):
+        zx.plot(x,y)
+        ax.set_title(title)
+        ax.xaxis.set_visible(False)
+        ax.set_ylim([min(y)=np.std(y),max(y) + np.std(y)])
+        ax.set_xlim([min(x), max(y)])
+        ax.grid(True)
 
     
 
